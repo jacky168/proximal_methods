4e117504697dee65f86df0e5a868236dd8c02dbb
Simon Matet <simon.matet@polytechnique.edu>
2015-04-01 14:43:33 +0200

    Implemented fast proximal gradient descent

--- main.py
+++ main.py
@@ -26,12 +26,15 @@ points, X, Y, points_ref, X_ref, Y_ref = generate_signal.generate(n, p)
 f = dual_elanet.ElaNet_Dual_With_Operator(X, alpha)
 g = dual_elanet.Scalar_Product(Y)
 A, B, C = np.linalg.svd(X)
+
 grad_lip_cte = B[0]**2 /alpha# Largest singular value of the kernel
 reg_path = prox_descent.prox_descent(n, f, g, stop, grad_lip_cte)
-
 buff = np.dot(np.transpose(X), reg_path)
 reg_path = np.clip(-1*buff - 1/alpha, 0, np.inf) - np.clip (buff -1/alpha, 0, np.inf)
 
+reg_path3 = prox_descent.accelerated_prox_descent(n, f, g, stop, grad_lip_cte)
+buff = np.dot(np.transpose(X), reg_path3)
+reg_path3 = np.clip(-1*buff - 1/alpha, 0, np.inf) - np.clip (buff -1/alpha, 0, np.inf)
 
 g = primal_elanet.ElaNet_Function(5, 5*alpha)
 f = primal_elanet.Square_Loss_Function(X, Y)
@@ -42,6 +45,18 @@ reg_path2 = prox_descent.prox_descent(2*p, f, g, stop, grad_lip_cte)
 
 for i in range (stop):
     plt.plot(points_ref, np.dot(X_ref, reg_path[:,i]), 'b')
-    plt.plot(points_ref, np.dot(X_ref, reg_path2[:,i]), 'g')
+    plt.plot(points_ref, np.dot(X_ref, reg_path3[:,i]), 'g')
+    
+score = np.sum(np.square(np.dot(X_ref, reg_path) - np.dot(Y_ref, np.ones((1,stop)))), axis=0)
+score2 = np.sum(np.square(np.dot(X_ref, reg_path2) - np.dot(Y_ref, np.ones((1,stop)))), axis=0)
+score3 = np.sum(np.square(np.dot(X_ref, reg_path3) - np.dot(Y_ref, np.ones((1,stop)))), axis=0)
+
+"""
+plt.plot(np.matrix(range(stop)), score, 'b')
+plt.plot(np.matrix(range(stop)), score2, 'r')
+plt.plot(np.matrix(range(stop)), score3, 'g')
+"""
+plt.show()
+
 #plt.plot(points_ref, Y_ref, 'g')
 
--- prox_descent.py
+++ prox_descent.py
@@ -7,6 +7,7 @@ Created on Thu Mar 26 10:42:58 2015
 """
 
 import numpy as np
+import copy
 
 """ Interface for g function """
 class Base_g_Function (object):
@@ -67,4 +68,37 @@ def prox_descent (p, f, g, stop, grad_lip_cte=-1, eta=default_eta,ini=0):
         v1 = v2     
         reg_path[:,i] = v1
         
+    return reg_path
+    
+def accelerated_prox_descent (p, f, g, stop, grad_lip_cte=-1, eta=default_eta,ini=0):
+    if (ini==0):
+        v2 = np.transpose(np.matrix(np.zeros(p)))
+        v1 = np.transpose(np.matrix(np.zeros(p)))
+    else:
+        v2 = ini
+        v1 = np.transpose(np.matrix(np.zeros(p)))
+    
+    reg_path = np.matrix(np.zeros((p, stop)))
+    step = 1000        
+    for i in range (stop):
+        y = v2 + float(i)/float(i+3) * (v2 - v1)
+        print np.sum(np.abs((v2 - v1)))
+        v1 = v2
+        
+        current_grad = f.grad(y)
+        if (grad_lip_cte >= 0): # If we know the lipschitz constante
+            step = 1/grad_lip_cte
+            v2 = (1 - eta(i)) * y + eta(i) * g.prox(step, y - step * current_grad)
+        else: # If we don't : Line search ; to be implemented ; not working for now
+            current_f_value = f.value(y)
+            step = 1000        
+            while (True):
+                v2 = (1 - eta(i)) * y + eta(i) * g.prox(step, y - step * current_grad)
+                if (f.value(v2) <= current_f_value + np.dot(np.transpose(current_grad), v2 - y) + 1/2/step * np.linalg.norm(y - v2)**2):
+                    break
+                else:
+                    step = step * 0.8
+        
+        reg_path[:,i] = v2
+
     return reg_path
